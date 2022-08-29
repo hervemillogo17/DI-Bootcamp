1a74bd6dcddd9bf9b58ff63a56057356b03c55bf
unknown <hervemillogo17>
2022-08-29 20:57:08 +0000

    Exercice du jour

---
+++ Week2/Day1/Daily-challenge/Script.js
@@ -0,0 +1,16 @@
+//Exercice1
+
+let fruits = ["Banana", "Apples", "Oranges","Blueberries"];
+fruits.shift();
+fruits.sort();
+fruits.push("Kiwi");
+fruits.splice(0,1);
+fruits.reverse();
+console.log(fruits);
+
+
+
+//Exercice2
+
+let moreFruits = ["Banana", ["Apples",["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1]);
---
+++ Week2/Day1/ExerciceXP/Script.js
@@ -0,0 +1,13 @@
+//Exercice1
+
+let food = "Attiéké";
+let meal = "Déjeuné";
+console.log( "I eat " + food + " at every " + meal);
+
+
+//Exercice2
+
+let myWatchedSeriesLength = ["The scorpion","money heist", "friends","game of throne"];
+let myWatchedSeriesSentence = "I watched 3 series";
+console.log("n");
+console.log(myWatchedSeriesSentence + ":" + myWatchedSeriesLength );
